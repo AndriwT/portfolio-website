66e390be3619fa8492698461606d817b96461f46
Andriw Tapanes <andriwtapanes@gmail.com>
2021-11-01 11:04:54 -0400

    fixed projects descriptions

--- src/components/ProjectsInfo.js
+++ src/components/ProjectsInfo.js
@@ -1,25 +1,29 @@
 const projects = [
   {
     name: "Letty's Cakes Website",
-    description: "This is a description",
+    description:
+      "Website created for Letty's Cakes, a Venezuelan baking business. This website was created using React JavaScript, Node.js, express and MongoDB, for data storage. This website has full CRUD functionality, allowing the admin to create new desserts, display, edit and delete them, as well as allowing all other users to place orders for the admin to view and delete.",
     imageSrc: "/lettyscakes.png",
     url: "https://lettyscakes.netlify.app/",
   },
   {
     name: "Cat-astrophe",
-    description: "This is another project for testing",
+    description:
+      "Cat-astrophe is a short game were you get to play as Catrina, a destructive cat that is left alone at home while her owner goes to the store. Your objective in this game is to break all possible objects inside the house before the Human gets back home, but not all objects are destroyable, have fun finding out which ones you can destroy! This little web game was developed using JavaScript Canvas.",
     imageSrc: "/catastrophe.png",
     url: "https://andriwt.github.io/cat-astrophe/",
   },
   {
     name: "W/O Contaxt",
-    description: "This is another project for testing",
+    description:
+      "Without Contaxt is a messaging app that allows you to connect and chat in real time with any other user in the platform, just sign-up to create your profile and pick someone you find interesting to start a chat with! This messaging app was created using Socket.io, for real time message delivery, React Javascript, Node.js, express and MongoDB for data storage, including messages, which are stored based on each individual chat, allowing you to come back at a later time and view all sent messages inside each chat. Authentication is in place for user signup and login",
     imageSrc: "/wocontaxt.png",
     url: "https://withoutcontaxt.netlify.app/",
   },
   {
     name: "Treesitos",
-    description: "This is another project for testing",
+    description:
+      "Treesitos is a website that invites you to plant flora around your area and add it to the treesitos map! A little incentive to work towards a greener environment and leave your mark. This is a collaborative work made for Shellhacks 2021, were my collaboration involved backend structure, part of the front end development along with CSS and design. Created using React JavaScript, Node.js, express and MongoDB, with google maps API implementation.",
     imageSrc: "/treesitos.png",
     url: "https://rocky-shore-48903.herokuapp.com/home",
   },
