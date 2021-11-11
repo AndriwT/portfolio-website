102293802a3ff689ff3c7df9754d21654099d2fd
Andriw Tapanes <andriwtapanes@gmail.com>
2021-11-11 14:01:23 -0500

    adding animations

--- src/views/ProjectsPage.jsx
+++ src/views/ProjectsPage.jsx
@@ -1,18 +1,22 @@
 import ProjectCard from "../components/ProjectCard";
 import projects from "../components/ProjectsInfo";
+import React from "react";
+import Fade from "react-reveal/Fade";
 
 const ProjectsPage = () => {
   return (
     <div style={{ minHeight: "100vh", color: "white" }}>
-      <div>
-        <h1 className="projects-title">Projects</h1>
+      <h1 className="projects-title">Projects</h1>
+      <div className="projects-container">
         <div className="projects-mapping">
           {projects &&
             projects.map((project, i) => {
               return (
-                <div key={i} className="projects">
-                  <ProjectCard props={project} />
-                </div>
+                <Fade top>
+                  <div key={i} className="projects">
+                    <ProjectCard props={project} />
+                  </div>
+                </Fade>
               );
             })}
         </div>
